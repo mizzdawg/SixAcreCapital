ac3312d906c5ef89b0e21b72c01bba12b3fd9507
Max Carty <mcarty79@gmail.com>
2023-12-22 13:54:40 -0500

    Add IntersectionObserver js code to SAC project

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
+let scrollElements;
+
+window.addEventListener(
+    "load",
+    (event) => {
+        scrollElements = document.querySelectorAll('.scroll-animate');
+
+        createObserver();
+    },
+    false
+);
+
+function createObserver() {
+    let observer;
+
+    let options = {
+        root: null,
+        rootMargin: "0px",
+        threshold: .1
+    };
+
+    observer = new IntersectionObserver(handleIntersect, options);
+    scrollElements.forEach((el) => {
+        observer.observe(el);
+    })
+}
+
+function handleIntersect(entries, observer) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('slide-up');
+        }
+    });
+}
+
+
+
 const menuBtns = document.querySelectorAll('.menu-btn');
 const mobileMenu = document.querySelector('.nav-mobile-menu-container');
 
